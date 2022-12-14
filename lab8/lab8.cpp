ec55657792e53661731d54d83399f5230c850632
leonydew <leonydew@gmail.com>
2022-12-14 20:28:31 +0200

    lab8
    qwerty

---
+++ lab8/lab8.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+int main() {
+    unsigned short x;
+    std::cin >> x;
+    unsigned char* y = (unsigned char*)&x;
+    std::cout << (int)(*y) << std::endl;
+    std::cout << (int)(*(y+1)) << std::endl;
+    return 0;
+}
